33120cdab74171d686e7ed4e6a232cc7956d5132
Pradipta Kr. Banerjee <pradipta.banerjee@gmail.com>
2014-10-27 23:23:16 +0530

    Start the app in debug mode

--- hello.py
+++ hello.py
@@ -16,4 +16,4 @@ def hello():
 
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(port))
+    app.run(debug=True, host='0.0.0.0', port=int(port))
