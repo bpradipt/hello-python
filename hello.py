802ea2afcde17a41d9dd4b2311861386b3a854bb
Pradipta Kr. Banerjee <pradipta.banerjee@gmail.com>
2014-10-27 15:18:35 +0530

    Add Twilio SMS handling

--- hello.py
+++ hello.py
@@ -1,12 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, request, redirect
+import twilio.twiml
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-	return 'Hello World!'
+    """Respond to incoming calls with a simple text message."""
+    resp = twilio.twiml.Response()
+    resp.message("Hello, This is Pradipta's Twilio Acc")
+    return str(resp)
 
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(port))
+    app.run(host='0.0.0.0', port=int(port))
